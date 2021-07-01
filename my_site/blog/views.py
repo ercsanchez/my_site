592a5d875fef9b65a6786314b06d8fa3c981cebb
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-01 22:16:05 +0000

    add initial global templates: base.html, 404.html | add initial index url, view, and template for blog app

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+posts = [
+    {"user": "user1", "title": "post1", "text": "post 1 text here"},
+    {"user": "user2", "title": "post2", "text": "post 2 text here"},
+    {"user": "user3", "title": "post3", "text": "post 3 text here"},
+]
+
+
 # Create your views here.
+def index(request):
+    return render(request, "blog/index.html", )

1a075cfa304387433da984227b0d9f5e08d3b56d
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-02 03:03:24 +0000

    create template, view, and url for all posts and each singular post | create and use includes: header

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('posts/', views.all_posts, name='all_posts'),
+    path('posts/<str:title>', views.post, name='post'),
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,12 +1,35 @@
+from django.http import response
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.http import Http404
 
 posts = [
-    {"user": "user1", "title": "post1", "text": "post 1 text here"},
-    {"user": "user2", "title": "post2", "text": "post 2 text here"},
-    {"user": "user3", "title": "post3", "text": "post 3 text here"},
+    {"username": "user1", "title": "post1", "content": "post 1 text"},
+    {"username": "user2", "title": "post2", "content": "post 2 text"},
+    {"username": "user3", "title": "post3", "content": "post 3 text"},
 ]
 
 
 # Create your views here.
 def index(request):
-    return render(request, "blog/index.html", )
+    return render(request, "blog/index.html")
+
+
+def all_posts(request):
+    return render(request, 'blog/posts.html', {
+        "posts": posts
+    })
+
+
+def post(request, title):
+    try:
+        for post in posts:
+            if post['title'] == title:
+                context = {
+                    "username": post['username'],
+                    "title": post['title'],
+                    "content": post['content']
+                }
+                return render(request, "blog/post.html", context)
+    except:
+        raise Http404()

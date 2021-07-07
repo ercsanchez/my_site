50575c2e04e6a6dbedc5f610f17238422bdc94ae
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-07 04:46:34 +0000

    112. fix all-posts and post-detail pages to fetch post data from db

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -14,13 +14,14 @@ def starting_page(request):
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by('-date')
     return render(request, 'blog/all-posts.html', {
         "all_posts": all_posts
     })
 
 
 def post_detail(request, slug):
-    matching_post = next(post for post in all_posts if post["slug"] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/post-detail.html', {
-        "post": matching_post
+        "post": identified_post
     })

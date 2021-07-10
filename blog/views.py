05b275461358d2304161581bc9858dbc3ac33943
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-10 05:42:32 +0000

    172. handle comment form submission in the SinglePostView and post-detail template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from blog.forms import CommentForm
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView
+from django.views import View
 
 from .models import Post
 from .forms import CommentForm
@@ -25,12 +27,31 @@ class AllPostsView(ListView):
     context_object_name = 'all_posts'
 
 
-class SinglePostView(DetailView):
-    model = Post
-    template_name = 'blog/post-detail.html'
+class SinglePostView(View):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['post_tags'] = self.object.tags.all()
-        context['comment_form'] = CommentForm()
-        return context
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            'post': post,
+            'post_tags': post.tags.all(),
+            'comment_form': CommentForm()
+        }
+        return render(request, 'blog/post-detail.html', context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)  # creates a model instance but doesn't write to db yet
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse('post-detail-page', 
+                args=[slug]))
+
+        context = {
+            'post': post,
+            'post_tags': post.tags.all(),
+            'comment_form': CommentForm(request.POST)
+        }
+        return render(request, 'blog/post-detail.html', context)

dbdd3c7ba5b0d4e966430039188855e38c3728f8
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-10 02:45:51 +0000

    170. add a comment form

---
+++ blog/forms.py
@@ -0,0 +1,15 @@
+from blog.models import Comment
+from django import forms
+
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['post']
+        labels = {
+            'user_name': 'Your Name',
+            'user_email': 'Your Email',
+            'text': 'Your Comment'
+        }
